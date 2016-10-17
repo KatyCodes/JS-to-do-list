8d56d6033bffa205062e71d03c9e8ab96bd740e8
evan stewart and Katy Henning <katy@>
2016-10-17 16:26:54 -0700

    fresh brains

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,35 +7,33 @@ import { Task } from './task.model';
   <div class="container">
     <h1>My First Angular 2 App</h1>
     <pies></pies>
-    <task-list></task-list>
-    <div *ngIf="selectedTask">
-      <h1>Edit Task</h1>
-      <div>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedTask.description">
-      </div>
-      <div>
-        <label>Enter Task ID:</label>
-        <input [(ngModel)]="selectedTask.id">
-        <button (click)="finishedEditing()">Done</button>
-      </div>
-    </div>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
+    <new-task
+      (newTaskSender)="addTask($event)"
+    ></new-task>
   </div>
   `
 })
 
 export class AppComponent {
-    public masterTaskList: Task[] = [
-        new Task("Create To-Do List app.", 0),
-        new Task("Learn Kung Fu.", 1),
-        new Task("Rewatch all the Lord of the Rings movies.", 2),
-        new Task("Do the laundry.", 3)
-    ];
+  public masterTaskList: Task[] = [
+
+  ];
   selectedTask: Task = null;
   showDetails(clickedTask: Task) {
     this.selectedTask = clickedTask;
   }
   finishedEditing() {
     this.selectedTask = null;
+  }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
