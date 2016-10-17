a91687a25f0181314b3e69f4ef479e483bc526ef
evan stewart and Katy Henning <katy@>
2016-10-17 10:54:13 -0700

    add initial files

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
-  <h2>Is this thing on?</h2>
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+    <div *ngFor="let currentTask of tasks">
+      <h3>{{ currentTask.description }}</h3>
+      <button (click)="showDetails(currentTask)">Edit</button>
+    </div>
+    <h1>Edit Task</h1>
+    <div>
+      <label>Enter Task Description</label>
+      <input [(ngModel)] = "selectedTask.description">
+    </div>
+    <div>
+    <label>Enter Task ID:</label>
+    <input [(ngModel)]="selectedTask.id">
+    </div>
+  </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
+  selectedTask: Task = this.tasks[0];
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
 }
